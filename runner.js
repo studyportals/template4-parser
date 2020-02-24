3e5a2dc634647f6bd122f12a4a28136ec06178ca
Thijs Putman <thijs@studyportals.com>
2020-02-24 15:06:10 +0100

    Automate the testing of New.tp4
    Executing "npm run test" will now echo "Okay" if the reference JSON is
    equal to the newly generated JSON. Also, added "npm run build-vanilla"
    that uses the vanilla jison-package (instead of jison-gho) in an attempt
    to maintain backwards-compatibility.

--- runner.js
+++ runner.js
@@ -1,9 +1,18 @@
-var fs = require("fs");
+const fs = require("fs");
+const prettier = require("prettier");
 
 function exec(input) {
   return require("./src/template4.js").parse(input);
 }
 
-var template4 = fs.readFileSync("test/New.tp4", "utf8");
+const template4 = fs.readFileSync("test/New.tp4", "utf8");
+const reference = fs.readFileSync("test/New.json", "utf8");
 
-console.log(JSON.stringify(exec(template4)));
+let output = JSON.stringify(exec(template4));
+output = prettier.format(output, { parser: "json", endOfLine: "crlf" });
+
+if (output.trim() === reference.trim()) {
+  console.log("Okay!");
+} else {
+  console.log("NOT Okay! 🥺");
+}
