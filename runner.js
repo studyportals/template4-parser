b8c4fdf80bea2404c0f9e2acc9a10e13aabdddab
Thijs Putman <thijs@studyportals.com>
2020-02-24 17:00:27 +0100

    Enhance runner.js and update documentation
    Also, get rid off Prettier in ESLint - it gets in the way of checking
    the generated JavaScript (as that *definitely* doesn't comply with
    Prettier).

--- runner.js
+++ runner.js
@@ -8,11 +8,32 @@ function exec(input) {
 const template4 = fs.readFileSync("test/New.tp4", "utf8");
 const reference = fs.readFileSync("test/New.json", "utf8");
 
-let output = JSON.stringify(exec(template4));
+let output = "";
+
+try {
+  output = JSON.stringify(exec(template4));
+} catch (e) {
+  console.log(`${e.name}: ${e.message}`);
+}
 output = prettier.format(output, { parser: "json", endOfLine: "crlf" });
 
-if (output.trim() === reference.trim()) {
-  console.log("Okay!");
-} else {
-  console.log("NOT Okay! 🥺");
+switch (process.argv[2]) {
+  case "output":
+    console.log(output);
+    process.exit(0);
+
+    break;
+
+  case "test":
+    if (output.trim() === reference.trim()) {
+      console.log("Okay!");
+      process.exit(0);
+    } else {
+      console.log("NOT Okay! 🥺");
+      process.exit(1);
+    }
+
+    break;
 }
+
+process.exit(1);
